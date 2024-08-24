885ad17931fada6b8db78d47910dadced112b250
vanthuc1810 <vanthuc08888@gmail.com>
2024-08-24 16:29:06 +0700

    update project

--- src/main/java/com/haui/btl/demo/DemoApplication.java
+++ src/main/java/com/haui/btl/demo/DemoApplication.java
@@ -3,11 +3,15 @@ package com.haui.btl.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDateTime;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		LocalDateTime now = LocalDateTime.now();
+		System.out.println(now);
 	}
 
 }
